f5c359656f80cd6887c27beb8309775bdfa2a74e
Hossain K. M. <awarehossain@gmail.com>
2024-04-21 20:03:42 +0600

    cm-3

---
+++ src/assets/data/blogData.js
@@ -0,0 +1,46 @@
+import img01 from "../images/blogs/blog-1.jpg";
+import img02 from "../images/blogs/blog-2.jpg";
+import img03 from "../images/blogs/blog-3.jpg";
+
+const blogData = [
+  {
+    id: 1,
+    title: "The best way to drive cars",
+    author: "Hasan",
+    date: "12 Dec, 2020",
+    time: "9pm",
+    imgUrl: img01,
+    description:
+      "Driving is not just about getting from point A to point B; it's an art form that requires skill, awareness, and responsibility. Whether you're a seasoned driver or a novice behind the wheel, adopting the best practices can not only enhance your driving experience but also contribute to road safety. Here are some tips on the best way to drive cars: \n 1. Stay Focused: One of the cardinal rules of driving is to stay focused on the road. Avoid distractions such as texting, eating, or adjusting the radio while driving. Your full attention should be on the task at hand navigating safely through traffic. \n 2. Obey Traffic Laws: Traffic laws are in place for a reason to ensure the safety of all road users. Always obey speed limits, traffic signals, and road signs. Remember, reckless driving not only endangers yourself but also others sharing the road with you. \n 3. Practice Defensive Driving: Defensive driving involves anticipating potential hazards and reacting accordingly. Keep a safe distance from other vehicles, scan the road ahead for any obstacles, and be prepared to react quickly if a dangerous situation arises.",
+    quote:
+      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+  },
+
+  {
+    id: 2,
+    title: "If your car battery is down",
+    author: "Hasib",
+    date: "12 Dec, 2020",
+    time: "9pm",
+    imgUrl: img02,
+    description:
+      "If your car battery is down, it can quickly turn a routine day into a frustrating ordeal. Whether you're stranded in a parking lot or facing the inconvenience of a stalled vehicle, a dead battery can disrupt your plans and leave you feeling stranded. However, with the right knowledge and preparation, you can efficiently address this common automotive issue. From jump-starting your vehicle to seeking assistance from roadside services, there are several solutions to get you back on the road swiftly. Remember to carry jumper cables, a portable jump starter, or have access to a roadside assistance service to ensure you're always",
+    quote:
+      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+  },
+
+  {
+    id: 3,
+    title: "The best way to give trip",
+    author: "Sojib",
+    date: "12 Dec, 2020",
+    time: "9pm",
+    imgUrl: img03,
+    description:
+      "The best way to give a trip is to ensure it's a seamless blend of excitement, relaxation, and unforgettable experiences. From meticulously planning the itinerary to anticipating the needs and preferences of your travel companions, every detail counts. Start by researching destinations that cater to your interests and offer a balance of activities and attractions. Consider factors such as the weather, local culture, and accessibility when finalizing your plans. Once you've chosen your destination, focus on creating a well-rounded itinerary that includes must-see landmarks, hidden gems, and opportunities for leisure and relaxation. Whether you're exploring bustling cities, scenic landscapes, or exotic locales, prioritize immersive experiences that allow you to connect with the destination on a deeper level",
+    quote:
+      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+  },
+];
+
+export default blogData;
